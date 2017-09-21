74b70d24b57813ef6b8e24c5f911caab8b0fac4a
Rajesh <rajeshsingh520@gmail.com>
2017-09-21 16:18:33 +0530

    Create plugin-information.php
    This information is added at the top of the plugin and it is the basic description of the plugin

---
+++ plugin-information.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Name of the plugin 
+Plugin URI: http://piwebsolution.com/
+Description: Description of the plugin
+Version: 1.0.0
+Author: Mr. Rajesh M. Singh
+Author URI: http://100dollarswebsites.com/
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl.html
+Donate link: http://www.piwebsolution.com/
+*/
+?>
